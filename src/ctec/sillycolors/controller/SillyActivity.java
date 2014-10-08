66800362df926649abb4d03dc17ebe9d1f01e8c8
Brennan Litster <brennan@thatoneplace.net>
2014-10-08 13:17:01 -0600

    Creation of Repo

---
+++ src/ctec/sillycolors/controller/SillyActivity.java
@@ -0,0 +1,44 @@
+package ctec.sillycolors.controller;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class SillyActivity extends Activity
+{
+	
+	private Button appButton;
+	private TextView appText;
+	private RelativeLayout appLayout;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_silly);
+		
+		appButton = (Button) findViewById(R.id.someRandomButton);
+		appText = (TextView) findViewById(R.id.someTextStuff);
+		appLayout = (RelativeLayout) findViewById(R.id.appLayout);
+		
+		heyListen();
+	}
+	
+	private void heyListen()
+	{
+		appButton.setOnClickListener(new View.OnClickListener()
+		{
+			
+			@Override
+			public void onClick(View v)
+			{
+				appLayout.setBackgroundResource(R.color.kerriganPurple);				
+			}
+		});
+	}
+}
